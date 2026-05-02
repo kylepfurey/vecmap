39d66cd93140e07fa12672d87daaabf6a6266466
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2026-05-01 22:09:31 -0400

    Update allocator.c

--- allocator.c
+++ allocator.c
@@ -3,45 +3,25 @@
 // by Kyle Furey
 
 #include "allocator.h"
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
 
-bool allocator_new(allocator_t *self, size_t size) {
-    assert(self != NULL);
-    assert(size > 0);
-    // TODO
+bool allocator_contains(const allocator_t *self, const void *ptr) {
 }
 
-void *allocator_borrow(allocator_t *self, size_t size) {
-    assert(self != NULL);
-    if (size == 0) {
-        return NULL;
-    }
-    // TODO
+void allocator_new(allocator_t *self, void *buffer, size_t size) {
 }
 
-void *allocator_move(allocator_t *self, void *memory, size_t size) {
-    assert(self != NULL);
-    if (memory == NULL) {
-        return allocator_borrow(self, size);
-    }
-    if (size == 0) {
-        return NULL;
-    }
-    // TODO
+void *allocator_malloc(allocator_t *self, size_t size) {
 }
 
-void allocator_return(allocator_t *self, void *memory) {
-    assert(self != NULL);
-    if (memory == NULL) {
-        return;
-    }
-    // TODO
+void *allocator_calloc(allocator_t *self, size_t n, size_t size) {
 }
 
-void allocator_free(allocator_t *self) {
-    assert(self != NULL);
-    // TODO
+void *allocator_realloc(allocator_t *self, void *ptr, size_t size) {
+}
+
+void allocator_free(allocator_t *self, void *ptr) {
 }
